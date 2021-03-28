599b5693b3fd12047117b9353f55c355d131fc97
Michael <msjia@me.com>
2021-03-28 22:00:12 +0800

    initial add project

---
+++ src/test/java/com/selfstudy/gateway/MsscBreweryGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.selfstudy.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBreweryGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
